01de48a5391d2e560669d1e7ccf26c2b7e9fc5b6
flymyd <2663797538@qq.com>
2023-12-16 10:21:25 +0800

    test uart

--- circuitpython/main.py
+++ circuitpython/main.py
@@ -1,23 +1,29 @@
 import time
+import supervisor
 import usb_hid
 from absolute_mouse import Mouse
+import board
+import busio
 
-m = Mouse(usb_hid.devices)
+mouse = Mouse(usb_hid.devices)
 
-# mouse_abs accept value from 0 to 32767 for both X and Y
-# Note: Values are NOT pixels! 32767 = 100% (to right or to bottom)
-
-
+# 32767 = 100%
 def transpose(x, y):
     return ((x * 32767) // 2560, (y * 32767) // 1440)
 
-
 positions = [(1, 1), (1280, 720)]
-
+uart = busio.UART(board.GP0, board.GP1, baudrate=115200)
 while True:
-    for position in positions:
-        print("MOVE", position, transpose(*position))
-        m.move(*transpose(*position))
-        time.sleep(2)
-    # time.sleep(10)
-    break
+    if supervisor.runtime.serial_bytes_available:
+        value = input().strip()
+        if value == "":
+            continue
+        if value == "identify_pico":
+            print("pico_detected")
+        elif value == "114514":
+            for position in positions:
+                print("MOVE", position, transpose(*position))
+                mouse.move(*transpose(*position))
+                time.sleep(2)
+        else:
+            print("1919810")
--- driver.py
+++ driver.py
@@ -1,35 +1,55 @@
+import time
 import serial
 import pyautogui
 import serial.tools.list_ports
 
+baudrate = 115200
+
+def send_package(port, payload):
+    try:
+        ser = serial.Serial(port, baudrate=baudrate)
+        command = f'{payload}\n\r'.encode()
+        ser.write(command)
+        reply = b''
+        for _ in range(len(command)):
+            a = ser.read()
+        while True:
+            a = ser.read()
+            if a== b'\r':
+                break
+            else:
+                reply += a
+            time.sleep(0.01)
+        response = reply.decode("ascii")
+        ser.close()
+        return response
+    except serial.SerialException:
+        return ""
+
 def search_pico():
     ports = serial.tools.list_ports.comports()
     for port in ports:
-        try:
-            ser = serial.Serial(port.device, baudrate=115200, timeout=1)
-            ser.write("identify_pico".encode())  # 发送识别指令
-            response = ser.readline().decode().strip()
-            ser.close()
-            if response == "pico_detected":
-                return port.device
-        except serial.SerialException:
+        response = send_package(port.device, "identify_pico")
+        if response == "pico_detected":
+            return port.device
+        else:
             pass
     return None
 
 pico_port = search_pico()
 if pico_port:
-    print("树莓派 Pico已识别，位于串口设备：", pico_port)
-    # 配置串口参数
-    ser = serial.Serial(pico_port, 115200)
-    # 循环接收指令并返回屏幕坐标
-    while True:
-        if ser.in_waiting > 0:
-            data = ser.readline().decode().strip()
-            if data == "button_pressed":
-                # 获取屏幕坐标
-                x, y = pyautogui.position()
-                # 发送坐标给树莓派
-                ser.write(f"{x},{y}\n".encode())
+    print("Pico已识别，位于串口设备：", pico_port)
+    res = send_package(pico_port, "ACCEED")
+    print(res)
+    # while True:
+    #     if ser.in_waiting > 0:
+    #         data = ser.readline().decode().strip()
+    #         print(data)
+    #         if data == "button_pressed":
+    #             # 获取屏幕坐标
+    #             x, y = pyautogui.position()
+    #             # 发送坐标给树莓派
+    #             ser.write(f"{x},{y}\n".encode())
 else:
     print("未找到树莓派 Pico")
 
