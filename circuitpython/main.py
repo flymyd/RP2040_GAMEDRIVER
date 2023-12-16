e7cd23ddd773b7ce48f46b271967fd13bfe916af
flymyd <2663797538@qq.com>
2023-12-16 14:20:48 +0800

    code clean

--- circuitpython/main.py
+++ circuitpython/main.py
@@ -15,45 +15,47 @@ base_resolution = [2560, 1440]
 def transpose(x, y):
     return ((x * 32767) // base_resolution[0], (y * 32767) // base_resolution[1])
 
+button_map = {
+    "L": Mouse.LEFT_BUTTON,
+    "R": Mouse.RIGHT_BUTTON,
+    "M": Mouse.MIDDLE_BUTTON
+}
+
 uart = busio.UART(board.GP0, board.GP1, baudrate=115200)
+
 while True:
     if supervisor.runtime.serial_bytes_available:
         value = input().strip()
-        if value == "":
-            continue
-        # Help client to find device
-        if value == "IDENTIFY":
-            print("DETECTED")
-        # Set screen resolution
-        elif value.startswith("SETSCR:"):
-            values = value[7:].split(',')
-            if len(values) == 2:
-                try:
-                    base_resolution = [int(values[0]), int(values[1])]
-                except ValueError:
-                    print("Invalid width or height")
+        if value:
+            cmd, _, args = value.partition(':')
+            # Device find helper
+            if cmd == "IDENTIFY":
+                print("DETECTED")
+            # Set vScreen resolution
+            elif cmd == "SETSCR":
+                args = args.split(',')
+                if len(args) == 2:
+                    try:
+                        base_resolution = list(map(int, args))
+                    except ValueError:
+                        print("Invalid width or height")
+                else:
+                    print("Invalid SETSCR value, like SETSCR:2560,1440")
+            # MouseMove handler
+            elif cmd == "MM":
+                args = args.split(',')
+                if len(args) == 2:
+                    try:
+                        mouse.move(*transpose(int(args[0]), int(args[1])))
+                    except ValueError:
+                        print("Invalid xCoord or yCoord")
+                else:
+                    print("Invalid MouseMove value, like MM:1280,720")
+            # MouseClick handler
+            elif cmd == "MC":
+                if args in ("L", "R", "M"):
+                    mouse.click(button_map[args])
+                else:
+                    print("Invalid MouseClick value, like MC:L or R or M")
             else:
-                print("Invalid SETSCR value, like SETSCR:2560,1440")
-        # Mouse Move handler
-        elif value.startswith("MM:"):
-            values = value[3:].split(',')
-            if len(values) == 2:
-                try:
-                    mouse.move(*transpose(int(values[0]), int(values[1])))
-                except ValueError:
-                    print("Invalid xCoord or yCoord")
-            else:
-                print("Invalid MouseMove value, like MM:1280,720")
-        # Mouse Click handler
-        elif value.startswith("MC:"):
-            keycode = value[3:]
-            if keycode == "L":
-                mouse.click(Mouse.LEFT_BUTTON)
-            elif keycode == "R":
-                mouse.click(Mouse.RIGHT_BUTTON)
-            elif keycode == "M":
-                mouse.click(Mouse.MIDDLE_BUTTON)
-            else:
-                print("Invalid MouseClick value, like MC:L or R or M")
-        else:
-            print("UNHANDLED CMD")
+                print("UNHANDLED CMD")
